4d588466f16f207c2edf5e7cca356fa559429539
jaeuk Park <mertake7@naver.com>
2023-07-17 18:01:15 +0900

    update

---
+++ 02-browser/01-document/02.01.ex.js
@@ -0,0 +1,17 @@
+const elem = document.getElementById("age-table");
+      console.log(elem);
+
+      const elements = document.querySelectorAll("#age-table label");
+      console.log(elements);
+
+      const div = document.querySelector("#age-table td:first-child");
+      console.log(div);
+
+      let form = document.getElementsByName("search")[0];
+      document.querySelector('form[name="search"]');
+
+      form.getElementsByTagName("input")[0];
+      form.querySelector("input");
+
+      let inputs = form.querySelectorAll("input");
+      inputs[inputs.length - 1];
